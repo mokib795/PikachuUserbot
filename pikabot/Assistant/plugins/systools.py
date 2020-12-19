e9c6c13814d4b681b2d60d68e52b008ead0a95d9
Sachin Jangir <support@ItzSjDude.in>
2020-12-19 06:21:25 +0530

    Create systools.py

---
+++ pikabot/Assistant/plugins/systools.py
@@ -0,0 +1,34 @@
+"""**HEROKU MANAGER**\n\n
+{i}set|get|del var <VarName>
+**Usage**: set|get|del Heroku Vars\n
+{i}usage
+**Usage**: Shows Dynos usage\n
+{i}logs
+**Usage**: Sends your botlogs in current chat\n
+{i}restart
+**Usage**: Restarts Pikabot"""
+
+from Assist_Modules import _dyno, _logs, _restart, _vars
+
+@ItzSjDude(pika=True, pattern=r"(set|get|del) var(?: |$)(.*)(?: |$)([\s\S]*)")
+@pikatgbot('OwnSudo')
+async def _(var):
+    await _vars(var)
+
+
+@ItzSjDude(pika=True, pattern=r"usage(?: |$)")
+@pikatgbot('OwnSudo')
+async def _(dyno):
+    await _dyno(dyno)
+
+
+@ItzSjDude(pika=True, pattern="restart")
+@pikatgbot('OwnSudo')
+async def _(rstrt):
+    await _restart(rstrt)
+
+
+@ItzSjDude(pika=True, pattern=r"logs")
+@pikatgbot('OwnSudo')
+async def _(dyno):
+    await _logs(dyno)
