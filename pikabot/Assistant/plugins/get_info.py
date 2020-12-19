e27c53472409ea6e642b3dc9d49c0a3db1bd3ee7
Sachin Jangir <support@ItzSjDude.in>
2020-12-19 06:15:55 +0530

    Create get_info.py

---
+++ pikabot/Assistant/plugins/get_info.py
@@ -0,0 +1,20 @@
+"""
+{i}get_id <reply to msg/media> 
+**Usage**: Get ID of any Telegram media, Group, or any user\n
+{i}info @username/reply to user msg
+**Usage**: Get full User information
+"""
+
+from Assist_Modules import _getinfo, _getid
+
+@ItzSjDude(pika=True, pattern="info(.*)")
+@pikatgbot('AmIAdm')
+@pikatgbot('AdmOnly')
+async def _(event):
+    await _getinfo(event)
+
+@ItzSjDude(pika=True, pattern="get_id(.*)")
+@pikatgbot('AmIAdm')
+@pikatgbot('AdmOnly')
+async def _(event):
+    await _getid(event)
