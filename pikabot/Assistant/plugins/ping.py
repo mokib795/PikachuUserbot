464648d10fb24a198281536381064c820e8735a0
Sachin Jangir <support@ItzSjDude.in>
2020-12-20 08:41:12 +0530

    Create ping.py

---
+++ pikabot/Assistant/plugins/ping.py
@@ -0,0 +1,12 @@
+"""
+{i}pika
+**Usage**: Checks Ping Speed
+"""
+
+# Made by @ItzSjDude for Pikabot
+from Asst_modules import _ping
+
+
+@ItzSjDude(pika=True, pattern="pika$")
+async def _(event):
+    await _ping(event)
