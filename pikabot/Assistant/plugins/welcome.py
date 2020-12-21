755bccad247830b1b83c7490b3b1bdde8c5bad51
Sachin Jangir <support@ItzSjDude.in>
2020-12-21 16:40:46 +0530

    Create welcome.py

---
+++ pikabot/Assistant/plugins/welcome.py
@@ -0,0 +1,35 @@
+"""
+{i}setwelcome <welcome message> or reply to msg
+**Usage**: Sets the message as a welcome note in the chat
+{i}getwelcome 
+**Usage**: Shows current welcome note in the chat\n
+{i}delwelcome 
+**Usage**: Deletes the welcome note from current chat\n\n 
+**Available formats for formatting welcome Note**:
+`{mention}, {title}, {count}, {first}, {last}, {fullname}, {userid}, {username}, {my_first}, {my_fullname}, {my_last}, {my_mention}, {my_username}`
+"""
+
+from . import ChatAction, _welcome, del_welcm, get_welcm, set_wlcm, tgbot, pikatgbot
+
+
+@tgbot.on(ChatAction)
+async def _(_pika):
+    await _welcome(_pika)
+
+
+@ItzSjDude(pika=True, pattern=r"setwelcome(?: |$)(.*)")
+@pikatgbot("OwnSudo")
+async def _(_pika):
+    await set_wlcm(_pika)
+
+
+@ItzSjDude(pika=True, pattern="getwelcome$")
+@pikatgbot("OwnSudo")
+async def _(_pika):
+    await get_welcm(_pika)
+
+
+@ItzSjDude(pika=True, pattern="delwelcome$")
+@pikatgbot("OwnSudo")
+async def _(_pika):
+    await del_welcm(_pika)
