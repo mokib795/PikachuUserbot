434397ffc6792dcc54a279af82141120e2a3bfa2
Sachin Jangir <support@ItzSjDude.in>
2020-12-22 10:23:05 +0530

    Update login.py

--- pikabot/login.py
+++ pikabot/login.py
@@ -8,5 +8,12 @@
 # Please see < https://github.com/ItzSjDude/PikachuUserbot/blob/master/LICENSE >
 #
 # All rights reserved 
-
+from telethon import events 
 from pikabot.Event_Handlers.login_client import pika_login 
+
+async def pika_msg(_pika, text, _pika_=None, parse_mode=None, link_preview=None):
+  parse_mode = parse_mode or "md"; link_preview = link_preview or False
+  if _pika_ is None:
+      return await _pika.edit(text, parse_mode=parse_mode, link_preview=link_preview)
+  else:
+      return await _pika.reply(text, parse_mode=parse_mode,link_preview=link_preview)
