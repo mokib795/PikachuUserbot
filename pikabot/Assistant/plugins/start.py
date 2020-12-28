b0d8d5119798f06080be73dc2c5a642229788e9c
Sachin Jangir <support@ItzSjDude.in>
2020-12-28 07:22:24 +0530

    Create start.py

---
+++ pikabot/Assistant/plugins/start.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2020 by ItzSjDude@Github, < https://github.com/ItzSjDude/PikachuUserbot >.
+#
+# This file is part of < https://github.com/ItzSjDude/PikachuUserbot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# 
+# Please see < https://github.com/ItzSjDude/PikachuUserbot/blob/master/LICENSE >
+#
+# All rights reserved 
+
+from pikabot.sql_helper.botusers_sql import *
+from Asst_modules import telethon, os, Button, custom, events, GetFullUserRequest
+
+pic = os.environ.get("START_PIC", None)
+if pic:
+ pic = pic
+else:
+ pic = "https://telegra.ph/file/2178b5a5b578d9f84d2e8.jpg"
+
+@ItzSjDude(pattern='^/start', pika=True)
+async def start(event):
+    botuname = (await event.client.get_me()).username
+    replied_user = await event.client(GetFullUserRequest(event.sender_id))
+    fname = replied_user.user.first_name
+    pname = (await bot.get_me()).first_name
+    stext = f"Hi {fname}, my name is {botuname}\n\n<// I am {pname}'s Assistant / Group Management Bot with a lot of Special Features. //>\n\n**Powered By** [PikachuUserbot](t.me/PikachuUserbot)\nDeveloped By  @ItzSjDude"
+    if event.sender_id == bot.uid:
+        await tgbot.send_message(event.chat_id, "Hi Master, Your Assistant / Group Manager Bot is Here 🙋\n\nDo /help to Reveal all Commands", buttons=[[custom.Button.inline("Users List", data= "pikalist")], [custom.Button.url("Add Me to Group 👥", f"t.me/{botuname}?startgroup=true")],])
+    else:
+        if is_user_exist(event.sender_id):
+            pass
+        else: 
+            add_user(event.sender_id)
+        await tgbot.send_file(
+            event.chat_id,
+            file=pic,
+            caption=stext,
+            link_preview=False,
+            buttons=[
+                [custom.Button.inline("Deploy your Own PikaBot", data="pika8")],
+                [Button.url("Need Help ❓", "t.me/pikachuUserbotSupport")],
+            ],
+        )
+
+
+@tgbot.on(Pika_CallBack(data=re.compile(rb"pika1\((.+?)\)")))
+async def _(_pika):
+  await _pikacallback1(_pika)
+
+@tgbot.on(Pika_CallBack(data=re.compile(rb"pika2\((.+?)\)")))
+async def _(_pika):
+  await _pikacallback2(_pika)
+
+@tgbot.on(Pika_CallBack(data=re.compile(b"pika3")))
+async def _(_pika):
+  await _pikacallback3(_pika)
+
+@tgbot.on(Pika_CallBack(data=re.compile(b"pika4")))
+async def _(_pika):
+  await _pikacallback4(_pika)
+
+@tgbot.on(Pika_CallBack(data=re.compile(b"pika5(.*)")))
+async def _(_pika):
+  await _pikacallback5(_pika)
+
+@tgbot.on(Pika_CallBack(data=re.compile(b"pika6(.*)")))
+async def _(_pika):
+  await _pikacallback6(_pika)
+
+@tgbot.on(Pika_CallBack(data=re.compile(b"pika7(.*)")))
+async def _(_pika):
+  await _pikacallback7(_pika)
+
+@tgbot.on(Pika_CallBack(data=re.compile(b"pika8(.*)")))
+async def _(_pika):
+  await _pikacallback8(_pika)
+
