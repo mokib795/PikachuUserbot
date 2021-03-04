922321670b63583393816114a4d55f34a3fc23f0
Sachin Jangir <support@ItzSjDude.in>
2021-03-04 08:29:54 +0530

    Update gmute_sql.py

--- pikabot/sql_helper/gmute_sql.py
+++ pikabot/sql_helper/gmute_sql.py
@@ -20,7 +20,40 @@ class GMute(BASE):
         self.pika_id = pika_id 
 
 GMute.__table__.create(checkfirst=True)
+
+class GBan(BASE):
+    __tablename__ = "gban"
+    sender = Column(String(14), primary_key=True)
+    pika_id = Column(Numeric, primary_key=True)
+    reason = Column(UnicodeText)
+    def __init__(self, sender, pika_id):
+        self.sender = str(sender)
+        self.pika_id = pika_id 
+        self.reason = reason 
+
+GBan.__table__.create(checkfirst=True)
        
+def is_gbanned(sender_id):
+    try:
+        return SESSION.query(GBan).all()
+    except: 
+        return None
+    finally:
+        SESSION.close()
+
+
+def gban(sender, pika_id, reason):
+    adder = GBan(str(sender), pika_id, reason)
+    SESSION.add(adder)
+    SESSION.commit()
+
+
+def ungban(sender, pika_id):
+    rem = SESSION.query(GBan).get((str(sender), pika_id))
+    if rem:
+        SESSION.delete(rem)
+        SESSION.commit() 
+
 def is_gmuted(sender_id):
     try:
         return SESSION.query(GMute).all()
