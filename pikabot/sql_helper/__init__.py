cd72e9d5f6c2713cf3b1d486245f828da1294d89
Sachin Jangir <support@ItzSjDude.in>
2021-03-18 15:03:20 +0530

    Update __init__.py

--- pikabot/sql_helper/__init__.py
+++ pikabot/sql_helper/__init__.py
@@ -16,8 +16,8 @@ def start() -> scoped_session:
 
 
 try:
-    BASE = declarative_base()
-    SESSION = start()
+    Base = declarative_base()
+    PikaDB = start()
 except AttributeError as e:
     # this is a dirty way for the work-around required for #23
     print("DB_URI is not configured. Features depending on the database might have issues.")
